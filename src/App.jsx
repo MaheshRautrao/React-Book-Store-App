046f9ff690e5bcb2b005afa8a598f26aa03b3998
MaheshRautrao <101188065+MaheshRautrao@users.noreply.github.com>
2023-12-22 21:24:15 +0530

    added testimonials section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import DownloadAppSection from "./components/downloadAppSection/DownloadAppSection";
 import Footer from "./components/footer/Footer";
+import TestimonialCard from "./components/testimonials/TestimonialCard";
 
 function App() {
   return (
     <div>
+      <TestimonialCard />
       <DownloadAppSection />
       <Footer />
     </div>
---
+++ src/components/testimonials/TestimonialCard.jsx
@@ -0,0 +1,19 @@
+const TestimonialCard = ({ testimonial_person_name, testimonial_text }) => {
+  return (
+    <div className="relative flex flex-col gap-4 px-6 py-8 mx-4 shadow-lg rounded-xl dark:bg-gray-800 bg-primary/10">
+      <div>
+        <img src="" alt="" className="object-cover w-20 h-20 rounded-full" />
+      </div>
+      <div>
+        <div>
+          <p className="text-sm text-gray-500">{testimonial_text}</p>
+          <h1 className="text-xl font-bold text-black/80 dark:text-white">
+            {testimonial_person_name}
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialCard;
