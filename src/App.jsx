2211026ff223c83c210d1a96767d5d6c9f2fb633
MaheshRautrao <101188065+MaheshRautrao@users.noreply.github.com>
2023-12-23 22:46:42 +0530

    added library features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import DownloadAppSection from "./components/downloadAppSection/DownloadAppSection";
 import Footer from "./components/footer/Footer";
+import LibrarySection from "./components/librarySection/LibrarySection";
 import TestimonialCard from "./components/testimonials/TestimonialCard";
 
 function App() {
   return (
     <div>
+      <LibrarySection />
       <TestimonialCard />
       <DownloadAppSection />
       <Footer />
